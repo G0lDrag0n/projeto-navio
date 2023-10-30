1f8c5408f85417d16132bdfde37d37b654ea2e15
Arthur Berreta Varela <arthurberretavarela@gmail.com>
2023-10-29 21:24:29 -0300

    segundo commit

--- sketch.js
+++ sketch.js
@@ -4,24 +4,30 @@ var mar
 var naviomov
 
 function preload(){
-mar= loadImage("sea.png")
-naviomov= loadAnimation("ship-1.png", "ship-1.png", "ship-3.png", "ship-4.png")
+mar = loadImage("sea.png")
+naviomov = loadAnimation("ship-1.png", "ship-2.png", 
+                        "ship-3.png", "ship-4.png")
 }
 
+
 function setup(){
-  createCanvas(400,400);
-  sea= createSprite(0, 0, 400, 400)
-  sea.addImage("sea.png", mar)
-  sea.scale= 0.5
-  sea.velocityX= -3
-  navio= createSprite(200, 250, 160, 50)
-  navio.addAnimation("ship-1.png", "ship-1.png", "ship-3.png", "ship-4.png", naviomov)
-  navio.scale= 0.3
+ createCanvas(400,400);
+
+ sea = createSprite(0, 0, 400, 400)
+ sea.addImage(mar)
+ sea.scale = 0.5
+ sea.velocityX = -3
+
+ navio = createSprite(200, 250, 160, 50)
+ navio.addAnimation("naviomov",naviomov)
+ navio.scale= 0.3
 }
 
 function draw() {
   background("blue");
-    drawSprites();
-
+  drawSprites();
  
+  if(sea.x < 0){
+    sea.x = sea.width/4;
+  }
 }
